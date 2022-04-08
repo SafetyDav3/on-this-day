e664ec4fb1622532fc92884a144d962f313d699e
SafteyDav3 <talktomedw@gmail.com>
2022-04-07 20:51:13 -0500

    Header stlye

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,35 @@
 //api call for event, birth and death data
-var getDateData = function(month, day) {
-  fetch(`https://octoproxymus.herokuapp.com?secret=walrus&url=${encodeURIComponent(`https://today.zenquotes.io/api/${month}/${day}`)}`)
-  .then(function(response) {
-      console.log(response)
-      return response.json()
-  })
-  .then(function(dateData) {
-      console.log(dateData)
-  })
-}
+var getDateData = function (month, day) {
+  fetch(
+    `https://octoproxymus.herokuapp.com?secret=walrus&url=${encodeURIComponent(
+      `https://today.zenquotes.io/api/${month}/${day}`
+    )}`
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (dateData) {
+      console.log(dateData);
+    });
+};
 
 //api call for APOD for specific date from NASA api
-var getNasaData = function() {
-  var selectedMonth = 10
-  var selectedDay = 06
-  console.log(selectedMonth, selectedDay)
-  fetch(`https://api.nasa.gov/planetary/apod?date=2021-${selectedMonth}-${selectedDay}&api_key=3Jkz68futt7JqR6rwvf60CmRxO3fHkW5mgA7NQSK`)
-  .then(function(response) {
-      console.log(response)
-      return response.json()
-  })
-  .then(function(nasaData) {
-      console.log(nasaData)
-      getDateData(selectedMonth, selectedDay)
-  })
-}
+var getNasaData = function () {
+  var selectedMonth = 10;
+  var selectedDay = 06;
+  console.log(selectedMonth, selectedDay);
+  fetch(
+    `https://api.nasa.gov/planetary/apod?date=2021-${selectedMonth}-${selectedDay}&api_key=3Jkz68futt7JqR6rwvf60CmRxO3fHkW5mgA7NQSK`
+  )
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (nasaData) {
+      console.log(nasaData);
+      getDateData(selectedMonth, selectedDay);
+    });
+};
 
-getNasaData()
-
-
-
-
-
+getNasaData();
